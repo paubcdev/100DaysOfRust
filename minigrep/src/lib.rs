17481a83768fc22f59f6585dbb691f0a89236a36
paubcdev <blanescolomina.pau@gmail.com>
2022-11-21 23:30:18 +0100

    minigrep, part 3

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,37 @@
+use std::error::Error; // This module form the standard collection allows to fine-tune error handling.
+use std::fs; // and this fs module, which handles the filesystem manipulation operations
+
+// We're going to create a configuration structure to group the values of the config part in the parse function.
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    // this way it shows that query and file_path are related to the program and their purpose is to configure how the program works
+}
+
+/* Having main.rs handle both the reading and parsing of the progam is not ideal, since, if any error would rise, it would become really difficult to debug and fix.
+We'll create a new function that is going to handle the logic of the parsing section of the code. This will migrate in the future to a different file.
+Since the function now depends on the Config structure, we can make a method out of it.
+By using a method now, it becomes clear what the function does and what it depends on.
+*/
+
+impl Config  {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> { // the input is a string vector and the output is going to be the config structure. Error handling by means of Result.
+        if args.len() < 3 {
+            return Err("not enough arguments"); // checks for the length of the argument vector: if fewer than the required 2 are passed, it throws an error.
+        }
+
+        let query = args[1].clone(); // clone in this case is used to fix ownership issues.
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+        // this is just the previous snippet of code, but refactored in a standalone function, takes the query and the file path and returns it for later use.
+    }
+}
+
+// The logic is extracted from the main function and put into a separate run function, for readibility and modularity
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?; // the file is read and put into a string, using the read_to_string() function from the fs module
+    
+    println!("With text:\n{contents}");
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,13 +2,15 @@
 // Program, in this iteration, takes two arguments: the string we want to find, and the path to the file we want to search into.
 
 use std::env; // From the standard library of Rust, we will be using the env module, specifically the args function, which returns arguments passed via command line (in this case)
-use std::fs; // and the fs module, whiocj handles the filesystem manipulation operations
 use std::process;
+
+use minigrep::Config;
 
 fn main(){
     let args: Vec<String> = env::args().collect(); // by using .collect(), we're telling Rust to turn the iterator (values passed) into a vector, thus initializing the "args" variable
     // dbg!(args); | the debug function (also part of the standard library)
 
+    // This checks for errors that may appear in the build function, if they're found, it displays them, then stops.
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
@@ -16,35 +18,10 @@ fn main(){
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path) // the file is read and put into a string, using the read_to_string() function from the fs module
-        .expect("Error: should have been able to read file"); // the function, besides the filepath, also takes an error argument, to display if something unexpected happens. 
     
-    println!("With text:\n{contents}");
-}
-// We're going to create a configuration structure to group the values of the config part in the parse function.
-struct Config {
-    query: String,
-    file_path: String,
-    // this way it shows that query and file_path are related to the program and their purpose is to configure how the program works
-}
-
-/* Having main.rs handle both the reading and parsing of the progam is not ideal, since, if any error would rise, it would become really difficult to debug and fix.
-We'll create a new function that is going to handle the logic of the parsing section of the code. This will migrate in the future to a different file.
-Since the function now depends on the Config structure, we can make a method out of it.
-By using a method now, it becomes clear what the function does and what it depends on.
-*/
-
-impl Config  {
-    fn build(args: &[String]) -> Result<Config, &'static str> { // the input is a string vector and the output is going to be the config structure. Error handling by means of Result.
-        if args.len() < 3 {
-            return Err("not enough arguments"); // checks for the length of the argument vector: if fewer than the required 2 are passed, it throws an error.
-        }
-
-        let query = args[1].clone(); // clone in this case is used to fix ownership issues.
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-        // this is just the previous snippet of code, but refactored in a standalone function, takes the query and the file path and returns it for later use.
+    // This checks for any errors that may arise in the run function, displays them and then halts the execution. Similar to the method before.
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
